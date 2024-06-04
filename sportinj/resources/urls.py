e914748b35fce9507e7a6368a585931811ce7a98
alekseybeloded <belod-aleksej@yandex.ru>
2024-06-04 20:20:58 +0300

    refactor: replacement func views to class views

--- sportinj/resources/urls.py
+++ sportinj/resources/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from resources import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('team/<slug:team_slug>/', views.get_all_players_for_team, name='players'),
-    path('team/<slug:team_slug>/<slug:player_slug>/injuries/', views.injury, name='injuries'),
-    path('addplayer/', views.add_player, name='add_player'),
+    path('', views.HomePage.as_view(), name='home'),
+    path('team/<slug:team_slug>/', views.GetPlayersForTeam.as_view(), name='players'),
+    path('team/<slug:team_slug>/<slug:player_slug>/injuries/', views.GetInjuriesForPlayer.as_view(), name='injuries'),
+    path('addplayer/', views.AddPlayer.as_view(), name='add_player'),
     path('contacts/', views.contacts, name='contacts'),
     path('login/', views.login, name='login'),
 ]

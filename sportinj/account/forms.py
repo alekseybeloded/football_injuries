c34c09c4acfec0d9c816c014571a93b50b9a55b1
alekseybeloded <belod-aleksej@yandex.ru>
2024-06-13 10:13:52 +0300

    feat: add RegisterUserForm

--- sportinj/account/forms.py
+++ sportinj/account/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm
 
@@ -6,3 +7,24 @@ class LoginUserForm(AuthenticationForm):
     class Meta:
         model = get_user_model()
         fields = ['username', 'password']
+
+
+class RegisterUserForm(forms.ModelForm):
+    username = forms.CharField()
+    password = forms.CharField()
+    password2 = forms.CharField()
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'password2']
+
+    def clean_password2(self):
+        if self.cleaned_data['password'] != self.cleaned_data['password2']:
+            raise forms.ValidationError("Passwords don't match")
+        return self.cleaned_data['password']
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if get_user_model().objects.filter(email=email).exists():
+            raise forms.ValidationError('This email already exists')
+        return email

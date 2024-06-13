284798b0b5539c2c4f51347e060ea16284e84f66
alekseybeloded <belod-aleksej@yandex.ru>
2024-06-13 11:15:44 +0300

    feat: add registration

--- sportinj/account/views.py
+++ sportinj/account/views.py
@@ -1,9 +1,18 @@
 from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-from account.forms import LoginUserForm
+from account.forms import LoginUserForm, RegisterUserForm
 
 
 class LoginUser(LoginView):
     form_class = LoginUserForm
     template_name = 'account/login.html'
     extra_context = {'title': 'Авторизация'}
+
+
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'account/register.html'
+    extra_context = {'title': 'Регистрация'}
+    success_url = reverse_lazy('account:login')

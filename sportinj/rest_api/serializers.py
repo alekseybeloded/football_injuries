d30757bf0029b14e0aaf8f0f2ba4c0a18e5f731a
alekseybeloded <belod-aleksej@yandex.ru>
2024-08-20 16:47:36 +0300

    feat: change serializer for Team, add serializers for Player and Injury

--- sportinj/rest_api/serializers.py
+++ sportinj/rest_api/serializers.py
@@ -1,8 +1,22 @@
-from resources.models import Team
+from resources.models import Injury, Player, Team
 from rest_framework import serializers
 
 
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
-        fields = ('name',)
+        fields = ('name', 'description')
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    team = serializers.CharField(source='team.name', read_only=True)
+
+    class Meta:
+        model = Player
+        fields = ('name', 'description', 'team')
+
+
+class InjurySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Injury
+        fields = ('name', 'description')

f736a6a0fe4ff68be829c66fa30217fe52d57667
alekseybeloded <belod-aleksej@yandex.ru>
2024-08-20 16:52:13 +0300

    feat: add TeamViewSet, PlayerViewSet, InjuryViewSet

--- sportinj/rest_api/views.py
+++ sportinj/rest_api/views.py
@@ -1,9 +1,19 @@
-from resources.models import Team
-from rest_framework import generics
+from resources.models import Injury, Player, Team
+from rest_framework import viewsets
 
-from rest_api.serializers import TeamSerializer
+from rest_api.serializers import InjurySerializer, PlayerSerializer, TeamSerializer
 
 
-class TeamApiView(generics.ListAPIView):
+class TeamViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
+
+
+class PlayerViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
+
+class InjuryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Injury.objects.all()
+    serializer_class = InjurySerializer

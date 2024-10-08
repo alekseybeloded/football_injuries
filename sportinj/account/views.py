e175699bb9f99aeca76ded9fba5959c177d160b8
alekseybeloded <belod-aleksej@yandex.ru>
2024-10-08 23:33:24 +0300

    feat: Implement user confirmation process
    - Updated UserRegistrationView to set user as inactive upon registration and redirect to confirmation page.
    - Added UserConfirmRegisterView to handle user registration confirmation.
    - Implemented UserConfirmRegisterActivateView to activate user accounts via token.
    - Created views for registration success, failure, and already confirmed registration.

--- sportinj/account/views.py
+++ sportinj/account/views.py
@@ -1,9 +1,15 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth.views import LoginView, PasswordChangeView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
+from django.views.generic import CreateView, TemplateView, UpdateView, View
+from resources.utils import ExtraContextMixin
 
 from account import forms
 
@@ -18,7 +24,14 @@ class UserRegistrationView(CreateView):
     form_class = forms.UserRegistrationForm
     template_name = 'account/register.html'
     extra_context = {'title': 'Registration'}
-    success_url = reverse_lazy('account:login')
+    success_url = reverse_lazy('account:user_confirm_register')
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_active = False
+        user.request = self.request
+        user.save()
+        return HttpResponseRedirect(self.success_url)
 
 
 class UserProfileUpdateView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
@@ -40,3 +53,45 @@ class UserPasswordChangeView(SuccessMessageMixin, PasswordChangeView):
     success_url = reverse_lazy('account:password_change_done')
     template_name = 'account/password_change_form.html'
     title = "Password change"
+
+
+class UserConfirmRegisterView(ExtraContextMixin, TemplateView):
+    template_name = 'account/user_confirm_register.html'
+    title_page = 'Confirm register'
+
+
+class UserConfirmRegisterActivateView(View):
+    def get(self, request, uidb64, token):
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = get_object_or_404(get_user_model(), pk=uid)
+        except (TypeError, ValueError, get_user_model().DoesNotExist):
+            user = None
+
+        if (
+                user is not None and
+                default_token_generator.check_token(user, token) and
+                not user.is_active
+            ):
+            user.is_active = True
+            user.save()
+            return redirect('account:user_confirm_register_success')
+        elif user is not None and user.is_active:
+            return redirect('account:user_already_confirm_register')
+        else:
+            return redirect('account:user_confirm_register_fail')
+
+
+class UserConfirmRegisterSuccessView(ExtraContextMixin, TemplateView):
+    template_name = 'account/user_confirm_register_success.html'
+    title_page = 'Successful registration'
+
+
+class UserConfirmRegisterFailView(ExtraContextMixin, TemplateView):
+    template_name = 'account/user_confirm_register_fail.html'
+    title_page = 'Failed registration'
+
+
+class UserAlreadyConfirmRegisterView(ExtraContextMixin, TemplateView):
+    template_name = 'account/user_already_confirm_register.html'
+    title_page = 'Confirm registration'

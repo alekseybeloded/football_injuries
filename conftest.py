92f7906f9b710dc7c7f61c910f9c8466841a69bf
alekseybeloded <belod-aleksej@yandex.ru>
2024-10-11 23:02:11 +0300

    test: change user fixture

--- conftest.py
+++ conftest.py
@@ -7,13 +7,13 @@ from resources.models import Team, Player, Injury
 @pytest.fixture
 def user(db):
     def create_user(
-            username='valid_username',
-            email='valid@mail.com',
-            password='valid_password',
-            first_name='first_name',
-            last_name='last_name',
-            is_active=True,
-        ):
+        username='valid_username',
+        email='valid@mail.com',
+        password='valid_password',
+        first_name='first_name',
+        last_name='last_name',
+        is_active=True,
+    ):
         user = get_user_model()
         user = user.objects.create_user(
             username=username,
